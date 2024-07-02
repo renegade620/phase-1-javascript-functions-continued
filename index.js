bdee913c4e5299a2ddc58b8f369506901636dcec
Frankline Were <franklinegift@gmail.com>
2024-07-02 14:49:55 +0300

    add functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun());
+
+const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+console.log(mondayWork());
+
+function wrapAdjective(flairString="*") {
+    return function(adjective="special") {
+        return `You are ${flairString}${adjective}${flairString}!`;
+    }
+}
+
+console.log(wrapAdjective()("a dedicated programmer"));
